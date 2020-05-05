5c2c0173d4a51a9902b36ab225974256afcf9989
sololobo904 <abhishekjha4506@gmail.com>
2020-05-05 19:30:47 +0530

    components 1 added

---
+++ confusion/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar dark color = "primary">
+         <div className = "container">
+            <NavbarBrand href = "/">Let's do it</NavbarBrand>
+         </div>
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
